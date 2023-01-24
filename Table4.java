e5800d85ba9517e1d02f3b005e186f8e3a428d86
Alekhya Vuppu <84461939+alekhya559@users.noreply.github.com>
2023-01-24 15:05:54 +0530

    Add files via upload

---
+++ Table4.java
@@ -0,0 +1,13 @@
+package basics;
+import java.util.*;
+
+public class Table4 {
+
+	public static void main(String[] args) {
+         int n = 4;
+         for(int i=1;i<=10;i++) {
+        	 System.out.println(4 +" * " + i + " = " + 4*i ); 
+         }
+	}
+}
+
